570365bda45b8d09052bc6c3f8fa89587d62c38c
Monishka Sharma <71654585+Monishka19@users.noreply.github.com>
2021-12-31 11:47:33 +0530

    second commit
    Displays images of shows related to every search.

--- tvMaze.js
+++ tvMaze.js
@@ -4,8 +4,15 @@ form.addEventListener('submit', async function (e){
     console.log('SUBMITTED!')
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
-    console.log();
-    const img = document.createElement('IMG');
-    img.src = res.data[0].show.image.medium;
-    document.body.append(img)
+    makeImages(res.data)
 })
+const makeImages = (shows) => {
+    for(let result of shows) {
+        if(result.show.image){
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+        
+    }
+}
